2c5ecc0bc11d5e5b5a93e870d8ce3d761d8a3aba
Alex Trujillo <atrujilloochoa@gmail.com>
2022-06-17 13:27:09 -0400

    missing function to access cache data

--- src/assembler/AssemblyValsCache.hpp
+++ src/assembler/AssemblyValsCache.hpp
@@ -9,6 +9,9 @@ namespace polyfem
 	public:
 		void init(const bool is_volume, const std::vector<ElementBases> &bases, const std::vector<ElementBases> &gbases);
 		void compute(const int el_index, const bool is_volume, const ElementBases &basis, const ElementBases &gbasis, ElementAssemblyValues &vals) const;
+		const ElementAssemblyValues* access_cache_data() const{
+			return cache.data();
+		}
 
 		void clear()
 		{

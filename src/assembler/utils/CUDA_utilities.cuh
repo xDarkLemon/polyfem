002ab004757f45eb1b807a40886d10603e168569
Alex Trujillo <atrujilloochoa@gmail.com>
2022-06-17 13:10:49 -0400

    setting more CUDA utilities and linking cuBLAS

--- src/assembler/utils/CUDA_utilities.cuh
+++ src/assembler/utils/CUDA_utilities.cuh
@@ -4,21 +4,13 @@
 #include <stdio.h>
 
 #define NUMBER_THREADS  32
-/*
-template <typename T>
-T* ALLOCATE_GPU(T *d_A, int _size);
-
-template <typename T>
-void COPYDATATOGPU(T *d_A,T *A,int _size);
-
-template <typename T>
-void COPYDATATOHOST(T *A,T *d_A,int _size);
-*/
 
 template <typename T>
 T* ALLOCATE_GPU(T *d_A, int _size) {
   if (cudaMalloc((void **)&d_A, _size) != cudaSuccess)
   {
+      cudaError_t err = cudaGetLastError();        // Get error code
+		  printf("CUDA Error: %s\n", cudaGetErrorString(err));
       printf("Error allocating GPU\n");
       abort();
   }
@@ -30,6 +22,8 @@ template <typename T>
 void COPYDATATOGPU(T *d_A,T *A,int _size){
     if(cudaMemcpy(d_A, A, _size, cudaMemcpyHostToDevice) != cudaSuccess)
     {
+      cudaError_t err = cudaGetLastError();        // Get error code
+		  printf("CUDA Error: %s\n", cudaGetErrorString(err));
       printf("Error copying to GPU\n");
       abort();
     }
@@ -39,6 +33,8 @@ template <typename X>
 void COPYDATATOGPU(X *d_A,const X *A,int _size){
     if(cudaMemcpy(d_A, A, _size, cudaMemcpyHostToDevice) != cudaSuccess)
     {
+      cudaError_t err = cudaGetLastError();        // Get error code
+		  printf("CUDA Error: %s\n", cudaGetErrorString(err));
       printf("Error copying to GPU\n");
       abort();
     }
@@ -48,13 +44,34 @@ template <typename T>
 void COPYDATATOHOST(T *A,T *d_A,int _size){
     if(cudaMemcpy(A,d_A,_size,cudaMemcpyDeviceToHost) != cudaSuccess)
     {
+      cudaError_t err = cudaGetLastError();        // Get error code
+		  printf("CUDA Error: %s\n", cudaGetErrorString(err));
       printf("Error copying to CPU\n");
       abort();
     }
 }
 
+void CATCHCUDAERROR(void);
+
+template <typename T> 
+void mult_trace(T*,int,double*);
+
+template <typename T> 
+__global__ void multiplicative_trace(T* mat, int n, double* result);
+
+template <typename T>
+__device__ void mat_mul_cache(T*,T*, T*, int);
 
 /*
+void CATCHCUDAERROR(void){
+cudaError_t err = cudaGetLastError();        // Get error code
+	   if ( err != cudaSuccess )
+	   {
+		      printf("CUDA Error: %s\n", cudaGetErrorString(err));
+		      exit(-1);
+	   }
+}
+
 template <typename T>
 void CUDA_FREE(T *d_A){
 cudaFree(d_A);

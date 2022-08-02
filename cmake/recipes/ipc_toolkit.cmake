d0a4cccc97e41df4fc39552a3a06951bb4048ef6
AlexTru96 <atrujilloochoa@gmail.com>
2022-08-02 16:27:08 -0400

    adding temporary fix to ipc toolkit

--- cmake/recipes/ipc_toolkit.cmake
+++ cmake/recipes/ipc_toolkit.cmake
@@ -10,8 +10,8 @@ message(STATUS "Third-party: creating target 'ipc::toolkit'")
 include(FetchContent)
 FetchContent_Declare(
     ipc_toolkit
-    GIT_REPOSITORY https://github.com/ipc-sim/ipc-toolkit.git
-    GIT_TAG 913f3886a7c1c41e94ef6dcc2bda6215da92edbf
+    GIT_REPOSITORY https://github.com/AlexTru96/ipc-toolkit
+    GIT_TAG c5afdccdcc2637af6351072cb883e60b30f6b005
     GIT_SHALLOW FALSE
 )
 FetchContent_MakeAvailable(ipc_toolkit)

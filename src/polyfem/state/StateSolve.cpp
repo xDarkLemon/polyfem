46b0034c223c305c2f0054b0e5c04f95ebb3bb3d
AlexTru96 <atrujilloochoa@gmail.com>
2022-09-21 05:57:50 -0400

    switcher MultiThread/GPU minimize function

--- src/polyfem/state/StateSolve.cpp
+++ src/polyfem/state/StateSolve.cpp
@@ -641,7 +641,15 @@ namespace polyfem
 		std::shared_ptr<cppoptlib::NonlinearSolver<NLProblem>> nlsolver = make_nl_solver<NLProblem>();
 		nlsolver->setLineSearch(args["solver"]["nonlinear"]["line_search"]["method"]);
 		nl_problem.init(sol);
-		nlsolver->minimize_gpu(nl_problem, tmp_sol);
+#ifdef USE_GPU
+		nlsolver->minimize(nl_problem, tmp_sol);
+		//nlsolver->minimize_gpu(nl_problem, tmp_sol);
+#endif
+#ifndef USE_GPU
+		nlsolver->minimize(nl_problem, tmp_sol);
+#endif
+
+
 		json nl_solver_info;
 		nlsolver->getInfo(nl_solver_info);
 		solver_info.push_back({{"type", "rc"},
@@ -657,7 +665,13 @@ namespace polyfem
 		{
 			logger().debug("Lagging iteration {:d}", lag_i + 1);
 			nl_problem.init(sol);
-			nlsolver->minimize_gpu(nl_problem, tmp_sol);
+#ifdef USE_GPU
+			nlsolver->minimize(nl_problem, tmp_sol);
+			//nlsolver->minimize_gpu(nl_problem, tmp_sol);
+#endif
+#ifndef USE_GPU
+			nlsolver->minimize(nl_problem, tmp_sol);
+#endif
 
 			nlsolver->getInfo(nl_solver_info);
 			solver_info.push_back({{"type", "rc"},
@@ -941,7 +955,13 @@ namespace polyfem
 		std::shared_ptr<cppoptlib::NonlinearSolver<NLProblem>> nlsolver = make_nl_solver<NLProblem>();
 		nlsolver->setLineSearch(args["solver"]["nonlinear"]["line_search"]["method"]);
 		nl_problem.init(sol);
-		nlsolver->minimize_gpu(nl_problem, tmp_sol);
+#ifdef USE_GPU
+		nlsolver->minimize(nl_problem, tmp_sol);
+		//nlsolver->minimize_gpu(nl_problem, tmp_sol);
+#endif
+#ifndef USE_GPU
+		nlsolver->minimize(nl_problem, tmp_sol);
+#endif
 		json nl_solver_info;
 		nlsolver->getInfo(nl_solver_info);
 		solver_info.push_back({{"type", "rc"},
@@ -969,7 +989,13 @@ namespace polyfem
 			// Disable damping for the final lagged iteration
 			if (lag_i == friction_iterations - 1)
 				nl_problem.lagged_damping_weight() = 0;
-			nlsolver->minimize_gpu(nl_problem, tmp_sol);
+#ifdef USE_GPU
+			nlsolver->minimize(nl_problem, tmp_sol);
+			//nlsolver->minimize_gpu(nl_problem, tmp_sol);
+#endif
+#ifndef USE_GPU
+			nlsolver->minimize(nl_problem, tmp_sol);
+#endif
 
 			nlsolver->getInfo(nl_solver_info);
 			solver_info.push_back({{"type", "rc"},
